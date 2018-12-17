d860eea9934550863236ad580512da10838fb516
rkatanguri <revanthreddy.katanguri@oubt.org>
2018-12-16 17:30:01 -0800

    second COMMIT

---
+++ numOfWays.py
@@ -0,0 +1,17 @@
+def helper(data,k):
+    if k==0:
+        return 1
+
+    s = len(data) - k
+    if data[s] == '0':
+        return 0
+
+    result = helper(data, k-1)
+    if k>=2 and int(data[s:s+2]) <=26:
+        result +=helper(data, k-2)
+    return result
+
+def num_ways(data):
+    return helper(data, len(data))
+
+print(num_ways('210'))
